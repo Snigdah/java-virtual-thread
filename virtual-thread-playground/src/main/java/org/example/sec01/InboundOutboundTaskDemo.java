e3647b4f951c33b0b50dc472ac4fd24c7caea23b
K M Farhat Snigdah <farhatsnigdho1392@gmail.com>
2024-04-23 16:51:35 +0600

    Minor changes in task

--- virtual-thread-playground/src/main/java/org/example/sec01/InboundOutboundTaskDemo.java
+++ virtual-thread-playground/src/main/java/org/example/sec01/InboundOutboundTaskDemo.java
@@ -4,13 +4,13 @@ import java.util.concurrent.CountDownLatch;
 
 // Create Platform thread
 public class InboundOutboundTaskDemo {
-    private static final int MAX_PLATFORM = 10;
+    private static final int MAX_PLATFORM = 20;
     private static final int MAX_VIRTUAL = 20;
 
 
     public static void main(String[] args) throws InterruptedException {
-      //  platformThreadDemo1();
-         virtualThreadDemo3();
+        platformThreadDemo1();
+         //virtualThreadDemo3();
     }
 
 

ab063e1d1f1acbf15b67ea170464e3fc66239e86
K M Farhat Snigdah <farhatsnigdho1392@gmail.com>
2024-05-07 16:32:07 +0600

    Added synchronization

---
+++ virtual-thread-playground/src/main/java/org/example/sec05/Lec02Synchronization.java
@@ -0,0 +1,40 @@
+package org.example.sec05;
+
+import org.example.util.CommonUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Lec02Synchronization {
+    private static final Logger log = LoggerFactory.getLogger(org.example.sec01.Task.class);
+    private static final List<Integer> list = new ArrayList<>();
+
+    public static void main(String[] args) {
+
+        //demo(Thread.ofVirtual());
+        //demo(Thread.ofPlatform());
+
+        CommonUtils.sleep(Duration.ofSeconds(2));
+
+        log.info("list size: {}", list.size());
+    }
+
+    private static void demo(Thread.Builder builder){
+        for (int i = 0; i < 50; i++ ){
+            builder.start(() -> {
+                log.info("Task started. {}", Thread.currentThread());
+                for (int j = 0; j < 200; j++) {
+                    inMemoryTask();
+                }
+                log.info("Task ended. {}", Thread.currentThread());
+            });
+        }
+    }
+
+    private static synchronized void inMemoryTask(){
+        list.add(1);
+    }
+}
